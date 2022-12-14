d1edc23b308ce1e5a21b526d779c8daf76744b47
krzkraken <cristhian.mirandas@gmail.com>
2022-12-14 00:57:43 -0500

    one to one relations

--- 006_relations/onetoone/views.py
+++ 006_relations/onetoone/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Place, Restaurant
 
 # Create your views here.
 def create(request):
     # Creacion de elementos
-    return HttpResponse("Datos creados")
+    place = Place(name="lugar 1", address="calle demo")
+    place.save()
+
+    restaurant = Restaurant(place=place, number_off_employees=8)
+    restaurant.save()
+
+    return HttpResponse(restaurant.place.name)
